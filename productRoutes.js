42dd45e77578979aabe1067657463e0d2476e9c0
Navaneeth <mnavaneeth321@gmail.com>
2026-06-25 23:29:31 +0530

    Add files via upload

---
+++ productRoutes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const {
+  getProducts,
+  getProductById,
+  createProduct,
+  updateProduct
+} = require("../controllers/productController");
+
+router.get("/", getProducts);
+router.get("/:id", getProductById);
+router.post("/", createProduct);
+router.put("/:id", updateProduct);
+
+module.exports = router;
